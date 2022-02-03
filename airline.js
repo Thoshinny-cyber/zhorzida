9bafb5ae3c722821f41edfad475ac641b6fbe43b
unknown <sec19cs021@sairamtap.edu.in>
2022-02-03 12:13:08 +0530

    manufacturer form

---
+++ airline.js
@@ -0,0 +1,23 @@
+const signinForm = document.querySelector('#login_div');
+signinForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    //get user info 
+    const email = signinForm['email'].value;
+    const password = signinForm['password'].value;
+
+    //sign up the user 
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then(cred =>{
+        console.log(cred.user); 
+        window.alert("Logged In successfully!!");
+        const modal = document.querySelector('#sign-in');  
+        window.location.href="analytics.html";
+    })
+    .catch((error) => {
+        var errorCode = error.code; 
+        var errorMessage = error.message; 
+        window.alert("Invalid Email id");
+        signinForm.reset();
+    })
+});
